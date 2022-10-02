72358e9a7eb7552bbd02c1fde4ffe426cc6b0487
madyardwn <madyardwn@gmail.com>
2022-10-02 11:09:53 +0700

    second commit

--- no3/Main.java
+++ no3/Main.java
@@ -5,16 +5,17 @@ abstract class Sortable {
   public static void shell_sort(Sortable[] a) { // Shell sort body
     int n = a.length;
     int h = 1;
-    while (h < n/3) h = 3*h + 1;
+    while (h < n / 3)
+      h = 3 * h + 1;
     while (h >= 1) {
       for (int i = h; i < n; i++) {
-        for (int j = i; j >= h && a[j].compare(a[j-h]) < 0; j -= h) {
+        for (int j = i; j >= h && a[j].compare(a[j - h]) < 0; j -= h) {
           Sortable t = a[j];
-          a[j] = a[j-h];
-          a[j-h] = t;
+          a[j] = a[j - h];
+          a[j - h] = t;
         }
       }
-      h = h/3;
+      h = h / 3;
     }
   }
 }
@@ -49,7 +50,7 @@ class Employee extends Sortable {
   }
 }
 
-class Manager extends Employee{
+class Manager extends Employee {
   public Manager(String n, double s, int d, int m, int y) {
     super(n, s, d, m, y);
     secretaryName = "";
@@ -68,45 +69,32 @@ class Manager extends Employee{
 
 class Main {
   public static void main(String[] args) {
-    // Employee[] staff = new Employee[3];
-    // staff[0] = new Employee("Harry", 50000, 1, 1, 2000);
-    // staff[1] = new Employee("Carl", 40000, 1, 1, 2001);
-    // staff[2] = new Employee("Tony", 45000, 1, 1, 2002);
+
+    // EMPLOYEE :
     Employee[] staff = new Employee[3];
     staff[0] = new Employee("Antonio Rossi", 2000000, 1, 10, 1989);
-    staff[1] = new Employee("Maria Bianchi", 2500000, 1, 12, 1991);
-    staff[2] = new Employee("Isabel Vidal", 1900000, 1, 11, 1993);
+    staff[1] = new Manager("Maria Bianchi", 2500000, 1, 12, 1991);
+    staff[2] = new Employee("Isabel Vidal", 3000000, 1, 11, 1993);
 
     Sortable.shell_sort(staff);
-    // for (Employee e : staff)
-    //   e.raiseSalary(5);
+
+    for (Employee e : staff)
+      e.raiseSalary(5);
     for (Employee e : staff)
       e.print();
 
     System.out.println();
 
-    Employee[] staff2 = new Employee[3];
-    staff2[0] = new Employee("Antonio Rossi", 2000000, 1, 10, 1989);
-    staff2[1] = new Manager("Maria Bianchi", 2500000, 1, 12, 1991);
-    staff2[2] = new Employee("Isabel Vidal", 1900000, 1, 11, 1993);
-    Sortable.shell_sort(staff2);
-    int i;
-    // for (i = 0; i < 3; i++)
-    //   staff2[i].raiseSalary(5);
-    for (i = 0; i < 3; i++)
-      staff2[i].print();
-
-    System.out.println();
-
+    // MANAGER :
     Manager[] boss = new Manager[3];
-    boss[0] = new Manager("Antonio Rossi", 2000000, 1, 10, 1989);
-    boss[1] = new Manager("Maria Bianchi", 1500000, 1, 12, 1991);
-    boss[2] = new Manager("Isabel Vidal", 1900000, 1, 11, 1993);
+    boss[0] = new Manager("Achmadya", 2000000, 1, 10, 1989);
+    boss[1] = new Manager("Ridwan", 1500000, 1, 12, 1991);
+    boss[2] = new Manager("Ilyawan", 3000000, 1, 11, 1993);
 
     Sortable.shell_sort(boss);
-
+    for (Employee e : boss)
+      e.raiseSalary(5);
     for (Manager m : boss)
       m.print();
-
   }
 }
